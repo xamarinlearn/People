a06316296ecb5c1742d57ddec6c164d812d7e1af
jese.alvarado <jese.alvarado@CDS2UNIVO7>
2019-08-19 09:09:17 -0600

    Add project files.

---
+++ People/People/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace People.Models
+{
+    [Table("people")]
+  public  class Person
+    {
+        // SQLite.NET
+        // Propiedad!
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; } // ?
+
+        [MaxLength(250), Unique]
+        public string Name { get; set; }
+
+        [Unique]
+        public int DUI { get; set; }
+    }
+}
---
+++ People/People/PersonRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using People.Models;
+using SQLite;
+
+namespace People
+{
+  public  class PersonRepository
+    {
+        SQLiteConnection conn;
+        public string StatusMessage { get; set; }
+
+        public PersonRepository(string dbPath)
+        {
+            conn = new SQLiteConnection(dbPath);
+            conn.CreateTable<Person>();
+        }
+
+        public void AddNewPerson(string name, int dui)
+        {
+            int result = 0;
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
